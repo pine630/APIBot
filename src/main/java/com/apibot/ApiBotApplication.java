bb3411aac58db8bb5a3f69392eb86d133416cc73
C.G <pinetree@hotmail.co.jp>
2018-07-21 15:50:41 +0900

    springsample

---
+++ src/main/java/com/apibot/ApiBotApplication.java
@@ -0,0 +1,12 @@
+package com.apibot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBotApplication.class, args);
+	}
+}
---
+++ src/test/java/com/apibot/ApiBotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.apibot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiBotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
