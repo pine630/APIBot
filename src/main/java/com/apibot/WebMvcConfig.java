1293d92b088e8156f461781f90d73856297ed785
C.G <pinetree@hotmail.co.jp>
2018-07-24 08:35:59 +0900

    login sample

---
+++ src/main/java/com/apibot/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package com.apibot;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SuppressWarnings("deprecation")
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
