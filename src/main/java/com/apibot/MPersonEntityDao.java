8950bafab63275f7bcf8e911be446f94739103ce
FJ)BASE)ChengGang <fj3731iv@aa.jp.fujitsu.com>
2018-07-24 22:36:42 +0900

    base

---
+++ src/main/java/com/apibot/MPersonEntityDao.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2017 Komatsu Ltd. All rights reserved.
+ */
+
+package src.main.java.com.apibot;
+
+import org.seasar.doma.Dao;
+import org.seasar.doma.Select;
+import org.seasar.doma.boot.ConfigAutowireable;
+
+/**
+ * MPersonEntityDao
+ */
+@ConfigAutowireable
+@Dao
+public interface MPersonEntityDao {
+
+    /**
+     * @param apiPublishSettingId apiPublishSettingId
+     * @return the MApiPublishSettingEntity entity
+     */
+    @Select
+    MApiPersonEntity selectById(Long id);
+
+}
